4451ce170a175ca4f72b694ef5ffd9292578e2ee
fixingeye31 <40872171+fixingeye31@users.noreply.github.com>
2019-08-09 16:33:32 -0700

    Update Beginners - If and else.ps1

--- Beginners - If and else.ps1
+++ Beginners - If and else.ps1
@@ -1,33 +1,58 @@
-﻿$computers = Read-Host -prompt 'How many computers do you have?'
+[int]$computers = Read-Host -prompt 'How many computers do you have?'
+
+
+
 
 
 if($computers -gt 5)
 
 
+
+
+
 {
+
 	Write-Host "Make a datacenter out of those computers."
+
 }
+
+
 
 else
+
 {
 
+
+
 	Write-Host "You need more computers to create a datacenter."
+
 	$check = Read-Host -prompt 'Would you like to buy more computers?'
-}
 
 
 if($check -eq 'yes')
+
 {
-	Read-Host -prompt 'How many computers would you like to buy?'
-	
+
+	[int]$ExtraComputers = Read-Host -prompt 'How many computers would you like to buy?'
+
 	[int]$cost=([int]$computers+[int]$ExtraComputers)
-	
+
 	Write-Host "you can make something out of $cost computers total."
+
+
 
 }
 
+
+
 	
+
 elseif($check -eq 'no')
+
 {
+
 	Write-Host "Someday you will make it."
+
+}
+
 }
